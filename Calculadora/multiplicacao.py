15c1429ecc603d0d51f8b749bc73a3f45957eacf
Yuri de Sousa Dias <diasyuri752@gmail.com>
2026-03-19 13:33:40 +0000

    .

--- Calculadora/multiplicacao.py
+++ Calculadora/multiplicacao.py
@@ -1,6 +1,6 @@
-def multiplicacao ():
-  a = float(input('numero 1: '))
-  b = float(input('numero 2: '))
+def multiplicacaof ():
+  a = float(input('Numero 1: '))
+  b = float(input('Numero 2: '))
   multiplicacao = a * b
   return multiplicacao
 
